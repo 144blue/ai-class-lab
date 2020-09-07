f261b50fd9637a7ae86446ea31975f8236f15893
144blue <aletru321@gmail.com>
2020-09-06 19:13:51 -0500

    all done tic tac toe

--- 01-python/tic-tac-toe/tic_tac_toe.py
+++ 01-python/tic-tac-toe/tic_tac_toe.py
@@ -1,4 +1,5 @@
 import re
+import random
 
 _PLAYER = "player"
 _MACHINE = "machine"
@@ -13,8 +14,27 @@ class TicTacToeGame():
     self.is_game_over = False
     self.winner = None
 
-  def is_over(self): # TODO: Finish this function by adding checks for a winning game (rows, columns, diagonals)
-    return self.board.count(None) == 0
+  def is_over(self): # TODO: Finish this function by adding checks for a winning game (rows, columns, diagonals) 
+    
+    
+    if self.board[0]== self.board[1] and self.board[2]== self.board[1]and self.board[0]!= None:
+        self.is_game_over=True
+    elif self.board[0]== self.board[3] and self.board[3]== self.board[6]and self.board[0]!= None:
+        self.is_game_over=True
+    elif self.board[1]== self.board[4] and self.board[4]== self.board[7]and self.board[1]!= None:
+        self.is_game_over=True
+    elif self.board[2]== self.board[5] and self.board[5]== self.board[8]and self.board[2]!= None:
+        self.is_game_over=True
+    elif self.board[3]== self.board[4] and self.board[4]== self.board[5]and self.board[3]!= None:
+        self.is_game_over=True
+    elif self.board[6]== self.board[7] and self.board[7]== self.board[8]and self.board[6]!= None:
+        self.is_game_over=True
+    elif self.board[0]== self.board[4] and self.board[4]== self.board[8]and self.board[0]!= None:
+        self.is_game_over=True
+    elif self.board[6]== self.board[4] and self.board[4]== self.board[2]and self.board[6]!= None:
+        self.is_game_over=True
+        
+    return self.is_game_over
 
   def play(self):
     if self.turn == _PLAYER:
@@ -48,29 +68,55 @@ class TicTacToeGame():
     chosen_cell = self.player_choose_cell()
 
     self.board[chosen_cell] = _PLAYER_SYMBOL
+    self.is_over()
+    if self.is_game_over==True:
+        self.winner="h"
+    
 
   def machine_turn(self):
     # TODO: Implement this function to make the machine choose a random cell (use random module)
     # The result of this function should be that self.board now has one more random cell occupied
-
-    for i, cell in enumerate(self.board):
-      if cell is None:
-        self.board[i] = _MACHINE_SYMBOL
-        break
+    
+    i= 4
+    
+    if self.board[i] is not None: 
+        while True:
+            i= random.randint(0, 8)
+            if self.board[i] is None:
+                break
+    self.board[i]= _MACHINE_SYMBOL
+    
+    self.is_over()
+    if self.is_game_over==True:
+        self.winner="m"
+    
+# =============================================================================
+#     for i, cell in enumerate(self.board):
+#       if cell is None:
+#         self.board[i] = _MACHINE_SYMBOL
+#         break
+# =============================================================================
 
   def format_board(self):
     # TODO: Implement this function, it must be able to print the board in the following format:
     #  x|o| 
     #   | | 
     #   | | 
-    return self.board
+    print(self.board[0],self.board[1],self.board[2],sep='|')
+    print(self.board[3],self.board[4],self.board[5],sep='|')
+    print(self.board[6],self.board[7],self.board[8],sep='|')
+   
 
   def print(self):
     print("Player turn:" if self.turn == _MACHINE else "Machine turn:")
-    print(self.format_board())
+    self.format_board()
     print()
 
   def print_result(self):
     # TODO: Implement this function in order to print the result based on the self.winner
+    if self.winner=="m":
+        print("pierdes")
+    elif self.winner == "h":
+        print("ganas")
 
     pass
